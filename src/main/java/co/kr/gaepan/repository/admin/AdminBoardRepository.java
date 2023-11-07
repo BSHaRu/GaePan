a32f54eadf5401147a37c33493fd5b118dbef701
BSHaRu <suj0309@naver.com>
2023-11-07 21:31:39 +0900

    notice board db 연결 후 출력

---
+++ src/main/java/co/kr/gaepan/repository/admin/AdminBoardRepository.java
@@ -0,0 +1,9 @@
+package co.kr.gaepan.repository.admin;
+
+import co.kr.gaepan.entity.admin.GP_AdminBoardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AdminBoardRepository extends JpaRepository<GP_AdminBoardEntity, Integer> {
+}
