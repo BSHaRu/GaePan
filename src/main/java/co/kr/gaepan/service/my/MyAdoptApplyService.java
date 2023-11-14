27058d16daf5e861c15360b521dd38799bc1afc0
sungwoo0902 <rhtjddn2@gmail.com>
2023-11-14 17:47:19 +0900

    14일 작업 개인정보수정 초기 기능 구현 완료 - 나의 리뷰 페이지 구현 중

---
+++ src/main/java/co/kr/gaepan/service/my/MyAdoptApplyService.java
@@ -0,0 +1,20 @@
+package co.kr.gaepan.service.my;
+
+import co.kr.gaepan.dto.pet.PetAdoptApplyDTO;
+import co.kr.gaepan.mapper.my.MyAdoptApplyMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class MyAdoptApplyService {
+
+    private final MyAdoptApplyService myAdoptApplyService;
+
+    public PetAdoptApplyDTO insertApply(PetAdoptApplyDTO petAdoptApplyDTO) {
+
+        return myAdoptApplyService.insertApply(petAdoptApplyDTO);
+    }
+}
