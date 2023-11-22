3134fddd04655b25405cfd8ad2eae5358afd6bf5
sungwoo0902 <rhtjddn2@gmail.com>
2023-11-22 19:20:38 +0900

    22일 작업 마이페이지 메인 화면 계정별 입양일기 목록 출력 구현 완료 계정별 QnA 목록 출력 구현 완료
    마이페이지 입양 일기
    목록 출력 및 페이징 구현 완료
    
    마이페이지 최근 신청 현황
    목록 출력 및 페이징 구현 완료

--- src/main/java/co/kr/gaepan/mapper/my/MyIndexMapper.java
+++ src/main/java/co/kr/gaepan/mapper/my/MyIndexMapper.java
@@ -3,21 +3,18 @@ package co.kr.gaepan.mapper.my;
 import co.kr.gaepan.dto.board.BoardCateDTO;
 import co.kr.gaepan.dto.board.BoardTypeDTO;
 import co.kr.gaepan.dto.my.MyQnaDTO;
+import co.kr.gaepan.dto.pet.PetRegisterDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-import java.util.Map;
 
 @Mapper
-public interface MyQnaMapper {
-
-    // 페이징 - 전체 게시글 갯수 계산
-    int selectQnaCountTotal(String uid);
+public interface MyIndexMapper {
+    List<MyQnaDTO> selectQna(String currentUserUid);
 
     List<BoardCateDTO> findCname(BoardCateDTO boardCateDTO);
 
     List<BoardTypeDTO> findTname(BoardTypeDTO boardTypeDTO);
 
-    // 나의 리뷰 10개씩 출력
-    List<MyQnaDTO> select_all(Map<String, Object> parameters);
+    List<PetRegisterDTO> selectDiary(String currentUserUid);
 }
