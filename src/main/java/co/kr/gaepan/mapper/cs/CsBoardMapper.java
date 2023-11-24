713d271d2ce62c52ec482ba435f38a976ed2c274
hansan <hansan9611@gmail.com>
2023-11-24 15:43:37 +0900

    월요일작업

--- src/main/java/co/kr/gaepan/mapper/cs/CsBoardMapper.java
+++ src/main/java/co/kr/gaepan/mapper/cs/CsBoardMapper.java
@@ -4,12 +4,26 @@ import co.kr.gaepan.dto.board.BoardDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Mapper
 public interface CsBoardMapper { // interface선언
 
-    //
+    // ★ 매개변수 바인딩 하는법
+    // String group만 하니까 쿼리문 바인딩 안되서 이렇게 파라미터 이름대로 바인딩 해주자
     public List<BoardDTO> findByGroupAndCate(@Param("group") String group, @Param("cate") int cate, @Param("offset") int offset, @Param("limit") int limit);
     public int countByGroupAndCate(@Param("group") String group, @Param("cate") int cate);
+    public BoardDTO findByNo(@Param("bno") int bno);
+    public BoardDTO findByParent(@Param("bno") int bno);
+
+    public void insertInquiry(
+            @Param("uid") String uid,
+            @Param("title") String title,
+            @Param("content") String content,
+            @Param("group") String group,
+            @Param("cate") int cate,
+            @Param("type") int type,
+            @Param("regDate") LocalDateTime regDate // 타입 String 아니라 LocalDateTime
+            );
 }
