a89f7563f327183272f12c91d700e32706a58d2d
bin <fg012023@gmail.com>
2023-11-24 17:38:09 +0900

    11/24일

--- src/main/java/co/kr/gaepan/controller/pet/PetController.java
+++ src/main/java/co/kr/gaepan/controller/pet/PetController.java
@@ -6,6 +6,7 @@ import co.kr.gaepan.service.pet.PetBoardService;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.modelmapper.internal.bytebuddy.implementation.bytecode.Division;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -91,13 +92,14 @@ public class PetController {
     public String searchPets(@RequestParam String searchType,
                              @RequestParam String key,
                              @RequestParam(defaultValue = "1") String pg,
+                             @RequestParam(defaultValue = "1") int division,
                              Model model) {
         log.info("searchType : " + searchType);
         log.info("key : " + key);
 
         int currentPage = petBoardService.getCurrentPage(pg);
 
-        int totalPages = petListMapper.searchPetsCount(searchType, key);
+        int totalPages = petListMapper.searchPetsCount(searchType, key, division);
 
         int lastPage = petBoardService.getLastPageNum(totalPages);
 
@@ -110,7 +112,7 @@ public class PetController {
         int pageGroupStart = result[0];
         int pageGroupEnd = result[1];
 
-        List<PetRegisterDTO> petList = petBoardService.searchPets(searchType, key, startNum);
+        List<PetRegisterDTO> petList = petBoardService.searchPets(searchType, key, startNum, division);
         model.addAttribute("petList", petList);
         model.addAttribute("currentPage", currentPage);
         model.addAttribute("lastPage", lastPage);
@@ -119,6 +121,7 @@ public class PetController {
         model.addAttribute("pageStartNum", pageStartNum + 1);
         model.addAttribute("searchType", searchType);
         model.addAttribute("key", key);
+        model.addAttribute("division", division);
 
         log.info("petList : " + petList);
 
--- src/test/java/co/kr/gaepan/mapper/pet/PetListMapperTest.java
+++ src/test/java/co/kr/gaepan/mapper/pet/PetListMapperTest.java
@@ -19,11 +19,11 @@ public class PetListMapperTest {
         String searchType = "tName";
         String key = "말티즈";
         int startNum = 1;
+        int division = 1;
 
-        List<PetRegisterDTO> result = petListMapper.searchPets(searchType, key, startNum);
+        List<PetRegisterDTO> result = petListMapper.searchPets(searchType, key, startNum, division);
 
         System.out.println("result: " + result);
-
 
     }
 
